5212593f5280fb294a55a1b40a082cdf4696bc26
S1gil0 <130371476+S1gil0@users.noreply.github.com>
2023-04-17 04:51:27 -0400

    Add files via upload

--- Pop.cs
+++ Pop.cs
@@ -4,33 +4,74 @@ using Oxide.Core;
 using Oxide.Core.Libraries;
 using Oxide.Core.Plugins;
 using Rust;
+using Oxide.Core.Libraries.Covalence;
 
 namespace Oxide.Plugins
 {
-    [Info("Pop", "Sigilo", "1.0.0")]
+    [Info("Pop", "Sigilo", "1.1.0")]
     [Description("Displays the number of connected and connecting players.")]
 
-    public class Pop : RustPlugin
+    public class Pop : CovalencePlugin
     {
+        private string command;
+
         private void Init()
         {
             permission.RegisterPermission("pop.use", this);
+            LoadConfig();
+            if (!string.IsNullOrEmpty(command))
+            {
+                AddCovalenceCommand(command, "PopCommand");
+            }
         }
 
-        [ChatCommand("pop")]
-        private void PopCommand(BasePlayer player, string command, string[] args)
+        protected override void LoadDefaultConfig()
         {
-            if (!permission.UserHasPermission(player.UserIDString, "pop.use"))
+            Config["Command"] = "pop";
+        }
+
+        private void LoadConfig()
+        {
+            try
             {
-                SendReply(player, "You don't have permission to use this command.");
+                command = Config.Get<string>("Command");
+            }
+            catch
+            {
+                LoadDefaultConfig();
+            }
+
+            SaveConfig();
+        }
+
+        private void SaveConfig()
+        {
+            Config["Command"] = command;
+            Config.Save();
+        }
+
+        private void PopCommand(IPlayer player, string cmd, string[] args)
+        {
+            if (!permission.UserHasPermission(player.Id, "pop.use"))
+            {
+                player.Reply(lang.GetMessage("NoPermission", this, player.Id));
                 return;
             }
 
             int connectedPlayers = BasePlayer.activePlayerList.Count;
             int joiningPlayers = Network.Net.sv.connections.Count - connectedPlayers;
 
-            string message = string.Format("Players Online: {0}/{1} | Players Joining: {2}", connectedPlayers, ConVar.Server.maxplayers, joiningPlayers);
-            SendReply(player, message);
+            string message = string.Format(lang.GetMessage("PopMessage", this, player.Id), connectedPlayers, ConVar.Server.maxplayers, joiningPlayers);
+            player.Reply(message);
+        }
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                {"NoPermission", "You don't have permission to use this command."},
+                {"PopMessage", "Players Online: {0}/{1} | Players Joining: {2}"}
+            }, this);
         }
     }
 }
